8a86dfa7cd79cab33064f8c8f67773896a11afba
babyo77 <144552425+babyo77@users.noreply.github.com>
2023-10-25 03:20:59 +0530

    minor bug fixes

--- public/script.js
+++ public/script.js
@@ -4,42 +4,85 @@ const sendBtn = document.getElementById("sendBtn")
 const ChatConatiner = document.getElementById('ChatConatiner')
 
 socket.on("message", message => {
+    scrollToBottom()
+    
     const divElement = document.createElement('div');
-    divElement.id = 'sender';
-    divElement.classList.add('border', 'self-start', 'border-white', 'h-7', 'rounded-bl-lg', 'rounded-r-lg');
+    divElement.id = 'me';
+    divElement.classList.add('border', 'self-start', 'border-white', 'text-left', 'break-words', 'rounded-bl-lg', 'rounded-r-lg');
 
     const pElement = document.createElement('p');
-    pElement.classList.add('px-3');
+    pElement.classList.add('p-3');
+    pElement.id = "Message"
     pElement.textContent = message;
 
     divElement.appendChild(pElement);
 
     ChatConatiner.appendChild(divElement);
 
+    checkMessageLength()
+
 })
 
-sendBtn.addEventListener('click', () => {
+function SendMessage(){
     if (!inputMessage.value.trim()) {
 
     } else {
         const message = inputMessage.value
         socket.emit("message", message)
 
+        scrollToBottom()
+        
         const divElement = document.createElement('div');
-        divElement.id = 'sender';
-        divElement.classList.add('border', 'self-end', 'border-white', 'h-7', 'rounded-br-lg' , 'rounded-l-lg');
+        divElement.id = 'me';
+        divElement.classList.add('border', 'self-end', 'border-white', 'text-left', 'break-words', 'rounded-br-lg' , 'rounded-l-lg');
 
         const pElement = document.createElement('p');
-        pElement.classList.add('px-3');
+        pElement.id = 'Message';
+        pElement.classList.add('p-3');
         pElement.textContent = message;
 
 
         divElement.appendChild(pElement);
 
         ChatConatiner.appendChild(divElement);
-
+      
         inputMessage.value=""
-
+        checkMessageLength()
        
     }
+}
+
+SendMessage()
+
+
+sendBtn.addEventListener('click', () => {
+    SendMessage()
 })
+
+document.body.addEventListener('keydown',(e)=>{
+    if(e.keyCode==13){
+        e.preventDefault()
+       SendMessage()
+    }
+})
+
+function scrollToBottom() {
+    ChatConatiner.scrollTop = ChatConatiner.scrollHeight;
+}
+
+
+function checkMessageLength() {
+    const messages = document.querySelectorAll("#me"); // Adjust the class name as needed
+
+    messages.forEach(message => {
+        const content = message.querySelector("#Message");
+        if (content.textContent.length >= 16) {
+            message.classList.add("w-56");
+        }
+    });
+}
+
+
+checkMessageLength()
+
+
