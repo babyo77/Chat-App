e65fc1b37624eff380bc5a29bdde796d7e547e90
babyo77 <144552425+babyo77@users.noreply.github.com>
2023-10-27 00:50:55 +0530

    Testing

--- public/script.js
+++ public/script.js
@@ -3,8 +3,9 @@ const inputMessage = document.getElementById("inputMessage")
 const sendBtn = document.getElementById("sendBtn")
 const ChatConatiner = document.getElementById('ChatConatiner')
 
-socket.on("message", message => {
-
+socket.on("message", (message,userId)=> {
+    
+    const messageWithLinks = convertUrlsToAnchors(message);
     const divElement = document.createElement('div');
     divElement.id = 'container';
     divElement.classList.add('border', 'self-start', 'border-white', 'text-justify', 'break-words', 'rounded-bl-lg', 'rounded-r-lg','cursor-pointer');
@@ -12,7 +13,14 @@ socket.on("message", message => {
     const pElement = document.createElement('p');
     pElement.classList.add('p-2');
     pElement.id = "Message"
-    pElement.textContent = message;
+    pElement.innerHTML = messageWithLinks;
+
+    const userIdElement = document.createElement('p');
+    userIdElement.classList.add('text-gray-400', 'text-xs' , 'text-left' ,'p-1','-mb-2.5');
+    userIdElement.textContent = userId;
+
+    divElement.appendChild(userIdElement);
+
 
     divElement.appendChild(pElement);
 
@@ -24,11 +32,13 @@ socket.on("message", message => {
 })
 
 function SendMessage(){
+    const userId=localStorage.getItem('user')
     if (!inputMessage.value.trim()) {
 
     } else {
         const message = inputMessage.value
-        socket.emit("message", message)
+        const messageWithLinks = convertUrlsToAnchors(message);
+        socket.emit("message", message,userId)
         
         const divElement = document.createElement('div');
         divElement.id = 'container';
@@ -37,15 +47,20 @@ function SendMessage(){
         const pElement = document.createElement('p');
         pElement.id = 'Message';
         pElement.classList.add('p-2');
-        pElement.textContent = message;
+        pElement.innerHTML = messageWithLinks;
 
+        const userIdElement = document.createElement('p');
+    userIdElement.classList.add('text-gray-400', 'text-xs' , 'text-right' ,'p-1','-mb-2.5');
+    userIdElement.textContent = " You ";
+
+    divElement.appendChild(userIdElement);
 
         divElement.appendChild(pElement);
 
         ChatConatiner.appendChild(divElement);
-      
+        
         inputMessage.value=""
-
+       
         scrollToBottom()
         checkMessageLength()
        
@@ -54,10 +69,27 @@ function SendMessage(){
 
 SendMessage()
 
+function convertUrlsToAnchors(message) {
+    const urlRegex = /(https?:\/\/[^\s]+)/g;
 
+    return message.replace(urlRegex, (url) => {
+        if (isImageURL(url)) {
+            return `<a href="${url}" target="_blank"><img src="${url}" alt="Image Preview" class="chat-image" /></a>`;
+        } else {
+            return `<a href="${url}" target="_blank" class="text-blue-300">${url}</a>`;
+        }
+    });
+}
 sendBtn.addEventListener('click', () => {
     SendMessage()
 })
+
+function isImageURL(url) {
+    const imageExtensions = ['jpg', 'jpeg', 'png', 'gif', 'bmp'];
+    const urlParts = url.split('.');
+    const extension = urlParts[urlParts.length - 1].toLowerCase();
+    return imageExtensions.includes(extension);
+}
 
 inputMessage.addEventListener('keydown',(e)=>{
     if(e.key==='Enter'){
@@ -72,7 +104,7 @@ function scrollToBottom() {
 
 
 function checkMessageLength() {
-    const messages = document.querySelectorAll("#container"); // Adjust the class name as needed
+    const messages = document.querySelectorAll("#container"); 
 
     messages.forEach(message => {
         const content = message.querySelector("#Message");
@@ -83,6 +115,139 @@ function checkMessageLength() {
     
 }
 
+function setUserName(){
+    let userId;
+    try {
+        if(!localStorage.getItem('user')){
+            userId = prompt("Enter Your Name")
+            if(!userId.trim()){
+             location.reload()
+            }else{
+            socket.emit("set-user-id", userId)
+            localStorage.setItem("user",userId)
+            }}
+            else{
+                userId=localStorage.getItem('user')
+                socket.emit("set-user-id", userId)
+            }
+        
+    } catch (error) {
+        location.reload()
+        
+    }
+}
+
+setUserName()
+
+
+socket.on('user-disconnected', (userId) => {
+  const divElement = document.createElement('div');
+  divElement.id = 'container';
+  divElement.classList.add('border', 'self-start', 'border-white', 'text-justify', 'rounded-br-lg' , 'rounded-l-lg' ,'cursor-pointer');
+
+  const pElement = document.createElement('p');
+  pElement.id = 'Message';
+  pElement.classList.add('p-2');
+  pElement.textContent = "User Disconnectd " + userId;
+
+
+  divElement.appendChild(pElement);
+
+  ChatConatiner.appendChild(divElement);
+
+});
+
+socket.on('user-connected', (userId) => {
+    const divElement = document.createElement('div');
+    divElement.id = 'container';
+    divElement.classList.add('border', 'self-start', 'border-white', 'text-justify', 'rounded-br-lg' , 'rounded-l-lg' ,'cursor-pointer');
+  
+    const pElement = document.createElement('p');
+    pElement.id = 'Message';
+    pElement.classList.add('p-2');
+    pElement.textContent = "User Connected " + userId;
+  
+  
+    divElement.appendChild(pElement);
+  
+    ChatConatiner.appendChild(divElement);
+  
+  });
 
 checkMessageLength()
 
+
+let isTyping = false;
+let typingTimeout;
+
+function sendTypingStatus() {
+  let  userId= localStorage.getItem("user")
+    if (!isTyping) {
+        isTyping = true;
+        socket.emit("typing", true,userId);
+    }
+    clearTimeout(typingTimeout);
+    typingTimeout = setTimeout(() => {
+        isTyping = false;
+        socket.emit("typing", false,userId);
+    }, 222); 
+}
+
+inputMessage.addEventListener("input", sendTypingStatus);
+
+function displayTypingNotification(userId, isTyping) {
+
+    if (isTyping) {
+        const typingNotification = document.createElement('div');
+        typingNotification.classList.add('typing-notification') 
+        typingNotification.textContent = `${userId} is typing...`;
+        ChatConatiner.appendChild(typingNotification);
+        scrollToBottom()
+    } else {
+        const typingNotifications = ChatConatiner.querySelectorAll('.typing-notification');
+        typingNotifications.forEach((notification) => {
+                ChatConatiner.removeChild(notification);
+        });
+    }
+}
+
+socket.on("typing", (userId, isTyping) => {
+    displayTypingNotification(userId, isTyping);
+});
+
+
+
+const roomInput = document.getElementById('roomInput');
+const roomDisplay = document.getElementById('roomDisplay');
+const joinRoomBtn = document.getElementById('joinRoomBtn'); // Add a "Join Room" button
+
+// Function to join an existing room
+joinRoomBtn.addEventListener('click', () => {
+    if(roomInput.value.trim()){
+        roomName = roomInput.value
+        document.getElementById("rooms").style.display="none"
+        document.getElementById("Chatconatiner").style.display="flex"
+        ChatConatiner.style.display="flex"
+        joinRoom(roomName);
+    }else{
+    const roomName = generateRoomName();
+    document.getElementById("rooms").style.display="none"
+    document.getElementById("Chatconatiner").style.display="flex"
+    ChatConatiner.style.display="flex"
+    joinRoom(roomName);}
+});
+
+// Function to join a room
+function joinRoom(roomName) {
+    let userId = localStorage.getItem("user")
+    socket.emit('join-room', roomName,userId);
+    ChatConatiner.innerHTML = '';
+    roomDisplay.textContent = `Room ID: ${roomName}`;
+}
+
+// Function to generate a unique room name or ID
+function generateRoomName() {
+    return "room_" + Math.random().toString(36).substr(2, 9);
+}
+
+// ... (the rest of your code)
--- server.js
+++ server.js
@@ -1,27 +1,77 @@
-const express = require('express')
-const dotenv = require('dotenv').config()
-const http = require('http')
-const { Server } = require("socket.io");
-const app = express()
-const server = http.createServer(app) 
+const express = require('express');
+const dotenv = require('dotenv').config();
+const http = require('http');
+const { Server } = require('socket.io');
+const app = express();
+const server = http.createServer(app);
 const io = new Server(server);
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-app.set('view engine','ejs')
-app.use(express.static('public'))
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
-app.get('/',(req,res)=>{
-res.render('index')
-})
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
-io.on("connection", (socket) => {
-    socket.on("message",(message)=>{
-        socket.broadcast.emit("message",message)
-        console.log(`Message from Socket ${socket.id} ${message}`)
-    })
+const rooms = {};
+
+io.on('connection', (socket) => {
+  // Function to join a room
+  socket.on('join-room', (roomName, userId) => {
+    // Create the room if it doesn't exist
+    if (!rooms[roomName]) {
+      rooms[roomName] = { users: [] };
+    }
+
+    // Join the room
+    socket.join(roomName);
+
+    // Store the current room in the socket's properties
+    socket.room = roomName;
+
+    // Store the user ID in the socket's properties
+    socket.userId = userId;
+
+    // Add the user to the room's user list
+    rooms[roomName].users.push({ userId, socket });
+
+    // Notify other users in the room about the new connection
+    socket.broadcast.to(roomName).emit('user-connected', userId);
+
+    // Notify the newly connected user about other users in the room
+    const usersInRoom = rooms[roomName].users.map((user) => user.userId);
+    socket.emit('users-in-room', usersInRoom);
+
+    console.log(`User ${userId} connected to room ${roomName}`);
   });
 
-server.listen(PORT,()=>{
-    console.log(`Server Running at ${PORT}`)
-})
+  socket.on('message', (message, userId) => {
+    // Broadcast the message to the users in the same room
+    socket.broadcast.to(socket.room).emit('message', message, userId);
+  });
+  socket.on('typing', (isTyping,userId) => {
+    // Broadcast the "typing" event to other connected clients
+    socket.broadcast.to(socket.room).emit('typing', userId, isTyping);
+});
+
+  socket.on('disconnect', () => {
+    const roomName = socket.room;
+    const userId = socket.userId;
+
+    // Remove the user from the room's user list
+    if (rooms[roomName]) {
+      rooms[roomName].users = rooms[roomName].users.filter((user) => user.userId !== userId);
+    }
+
+    // Notify other users in the room about the disconnection
+    socket.broadcast.to(roomName).emit('user-disconnected', userId);
+
+    console.log(`User ${userId} disconnected from room ${roomName}`);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server Running at ${PORT}`);
+});
